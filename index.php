3726da979bb9f873c1dbc607eea0f34ad46ed479
nhdung0812 <Nhdung0812@gmail.com>
2021-01-02 23:51:03 +0700

    www
    sssss

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+       <link rel="stylesheet" href="css/style.css">
+       <title>Document</title>
+</head>
+<body>
+       <div class="container m-3 ">
+              <div class="m-auto text-center mb-3">
+                     <a class="" href="#">
+                            <img src="//placehold.it/150" class="rounded-circle">
+                     </a>
+              </div>
+              <div class="row m-auto text-center mb-2">             
+                     <h1>Nguyễn Huy Dũng</h1>
+              </div>
+              <div class="row text-center m-auto p-2 " style="width: 47%;">   
+                     <div class="card " style="border-radius: 12px;">
+                            <div class="row">
+                                   <a href="#" class="btn m-0 p-0 ">
+                                          <div class="card-body d-flex align-items-center m-0 p-0" >      
+                                                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKcYDJY4hyF7SsLPaKRxuPCbOs8n-jcxUEW3m2yKyCp3l9eZv-bw1pce7XDuFWs1cYAtUV49b5Us7en9yW7GEmdXJRwJXbwA5x7w&usqp=CAU&ec=45750088" width="70rem" height="" class="" alt="">
+                                                 <h3>Facebook</h3>
+                                          </div>
+                                   </a>
+                            </div>
+                     </div>          
+                    
+              </div>
+              <div class="row text-center m-auto p-2 " style="width: 47%;">   
+                     <div class="card " style="border-radius: 12px;">
+                            <div class="row">
+                                   <a href="#" class="btn m-0 p-0 ">
+                                          <div class="card-body d-flex align-items-center m-0 p-0" >      
+                                                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKcYDJY4hyF7SsLPaKRxuPCbOs8n-jcxUEW3m2yKyCp3l9eZv-bw1pce7XDuFWs1cYAtUV49b5Us7en9yW7GEmdXJRwJXbwA5x7w&usqp=CAU&ec=45750088" width="70rem" height="" class="" alt="">
+                                                 <h3>Facebook</h3>
+                                          </div>
+                                   </a>
+                            </div>
+                     </div>          
+                    
+              </div>
+              <div class="row text-center m-auto p-2 " style="width: 47%;">   
+                     <div class="card " style="border-radius: 12px;">
+                            <div class="row">
+                                   <a href="#" class="btn m-0 p-0 ">
+                                          <div class="card-body d-flex align-items-center m-0 p-0" >      
+                                                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKcYDJY4hyF7SsLPaKRxuPCbOs8n-jcxUEW3m2yKyCp3l9eZv-bw1pce7XDuFWs1cYAtUV49b5Us7en9yW7GEmdXJRwJXbwA5x7w&usqp=CAU&ec=45750088" width="70rem" height="" class="" alt="">
+                                                 <h3>Facebook</h3>
+                                          </div>
+                                   </a>
+                            </div>
+                     </div>          
+                    
+              </div>
+       </div>
+       <script src=""></script>
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
+</html>
